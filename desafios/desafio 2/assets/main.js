0ebb93e19e22a37511061895a9ce06baa9e0dfc8
leonardo huaman <leonardohuaman99@gmail.com>
2025-04-21 19:03:23 -0300

    se realizo todo el trabajo en 10 min :)

---
+++ desafios/desafio 2/assets/main.js
@@ -0,0 +1,12 @@
+
+console.log("trabajito tralala")
+
+const pombreProducto = "alfajor";
+const precioUnitario = 250;
+
+let cantidadDeseada = prompt ("Cuantas unidades de alfajores desea comprar?");
+
+const Resultado = precioUnitario * cantidadDeseada;
+
+alert("en total le costaria " + Resultado);
+
