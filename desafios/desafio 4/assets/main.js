ece0539133367703fca8c1501983ff3f1f65b1d2
leonardo huaman <leonardohuaman99@gmail.com>
2025-05-04 19:40:02 -0300

    se realizo el desafio 4

---
+++ desafios/desafio 4/assets/main.js
@@ -0,0 +1,13 @@
+
+let Resultado;
+
+
+function sumarProductos(precioUnitario,cantidadDeseada){
+    Resultado = precioUnitario*cantidadDeseada;
+    console.log("en total le costaria " + Resultado);
+}
+
+sumarProductos(1000,prompt ("Cuantas unidades de mantecol quiere comprar?"));
+
+
+
